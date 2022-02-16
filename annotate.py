d92682de48c0b7002b00ea12999d9add53a2ac5f
cbadenes <cbadenes@gmail.com>
2022-02-16 16:13:24 +0100

    handle page

--- annotate.py
+++ annotate.py
@@ -12,6 +12,7 @@ import sys
 import requests
 import multiprocessing as mp
 from datetime import datetime
+from random import random
 
 if __name__ == '__main__':
 
@@ -27,22 +28,38 @@ if __name__ == '__main__':
     solr = pysolr.Solr(url, always_commit=True, timeout=120)
     print("reading paragraphs from:",url)
 
-    print("Number of processors: ", mp.cpu_count())
-    pool = mp.Pool(mp.cpu_count())
+    #num_parallel = mp.cpu_count()
+    num_parallel = 4
+    print("Number of processors: ", num_parallel)
+    pool = mp.Pool(num_parallel)
 
     t = time.time()
-    documents = []
-    counter = 0
-    for doc in solr.search('*:*',sort='id ASC',cursorMark='*'):
+    finished = False
+    page = 0
+    size = 2
+    total = -1
+    max_size = -1
+    while (not finished) and (total <= max_size):
         try:
-            counter += 1
-            documents.append(doc)
-            if (len(documents) == 100):
-                print("[",datetime.now(),"]",counter,"paragraphs: annotating 100")
-                paragraphs = pool.map(annotators.parse,documents)
-                solr.add(paragraphs)
-                documents = []
+            results = solr.search('*:*',rows=size,start=page,sort='id ASC')
+            if (max_size < 0):
+                max_size = results.hits
+            num_found = len(results)
+            next_cursor = results.nextCursorMark
+            print("[",datetime.now(),"]","Page:",page,"Num_Found:", num_found,"Total:",total,"Hits:",max_size)
+            finished = len(results) < size
+            page += 1
+            documents = []
+            for doc in results:
+                total += 1
+                documents.append(doc)
+            #print("[",datetime.now(),"]",total,"paragraphs: annotating the latest",size)
+            paragraphs = pool.map(annotators.parse,documents)
+            solr.add(paragraphs)
         except Exception as e:
-            print("Error saving in solr:",e)        
+            print("Error reading from solr:",e)
+            delay_in_secs = random()*100
+            print("waiting for ", delay_in_secs, " secs ..")
+            time.sleep(delay_in_secs)
     print('Time to annotate paragraphs: {} mins'.format(round((time.time() - t) / 60, 2)))
     print("Total Annotations:",counter)
