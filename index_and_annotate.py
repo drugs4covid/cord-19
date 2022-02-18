e4737986e430172fc1b8cd9d0641b046c02297b9
cbadenes <cbadenes@gmail.com>
2022-02-18 16:55:24 +0100

    handle exception

--- index_and_annotate.py
+++ index_and_annotate.py
@@ -18,8 +18,8 @@ if __name__ == '__main__':
 
     # Create a client instance. The timeout and authentication options are not required.
     server = 'http://localhost:8983/solr'
-    solr_papers = pysolr.Solr(server+'/cord19-papers', always_commit=True, timeout=50)
-    solr_paragraphs = pysolr.Solr(server+'/cord19-paragraphs', always_commit=True, timeout=50)
+    solr_papers = pysolr.Solr(server+'/cord19-papers', always_commit=True, timeout=120)
+    solr_paragraphs = pysolr.Solr(server+'/cord19-paragraphs', always_commit=True, timeout=120)
 
 
     if (len(sys.argv) != 2):
@@ -34,34 +34,38 @@ if __name__ == '__main__':
     total_files = len(files)
     print(total_files,"files available")
 
-    num_parallel = 18
+    num_parallel = 10
     print("Number of processors: ", num_parallel)
     pool = mp.Pool(num_parallel)
 
-    increment = 100
+    increment = 10
     min_idx = 0
     max_idx = increment
     t = time.time()
     count_papers = 0
     count_paragraphs = 0
     while(min_idx<total_files):
-        print("[",datetime.now(),"]","indexing articles between:",min_idx,"-",max_idx)
-        results = pool.map(workers.parse_and_annotate,files[min_idx:max_idx] )
-        min_idx = max_idx
-        max_idx = min_idx + increment
-        papers = []
-        paragraphs = []
-        for result in results:
-            if ('paper' in result):
-                papers.append(result['paper'])
-            if ('paragraphs' in result):
-                paragraphs.extend(result['paragraphs'])
-        count_papers += len(papers)
-        print("Papers added:",count_papers)
-        solr_papers.add(papers)
-        count_paragraphs += len(paragraphs)
-        print("Paragraphs added:",count_paragraphs)
-        solr_paragraphs.add(paragraphs)
+        try:
+            print("[",datetime.now(),"]","indexing articles between:",min_idx,"-",max_idx)
+            results = pool.map(workers.parse_and_annotate,files[min_idx:max_idx] )
+            papers = []
+            paragraphs = []
+            for result in results:
+                if ('paper' in result):
+                    papers.append(result['paper'])
+                if ('paragraphs' in result):
+                    paragraphs.extend(result['paragraphs'])
+            count_papers += len(papers)
+            print("Papers added:",count_papers)
+            solr_papers.add(papers)
+            count_paragraphs += len(paragraphs)
+            print("Paragraphs added:",count_paragraphs)
+            solr_paragraphs.add(paragraphs)
+            min_idx = max_idx
+            max_idx = min_idx + increment
+        except Exception as e:
+            print("Error reading from solr:",e)
+
 
     pool.close()
     print('Time to parse articles: {} mins'.format(round((time.time() - t) / 60, 2)))
